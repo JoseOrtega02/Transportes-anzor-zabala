f3d4e2e234e069ccdb3a0f9af56dbe85390f1267
neytor <ortegajoseweb02@gmail.com>
2024-01-09 22:04:14 -0300

    performance map refactor

--- AyZTransportes/src/pages/Tarifa/Tarifa.tsx
+++ AyZTransportes/src/pages/Tarifa/Tarifa.tsx
@@ -1,5 +1,4 @@
-import React, { Suspense } from "react";
-
+import React, { Suspense, useCallback } from "react";
 import InputFrom from "./components/Input/InputFrom";
 import InputTo from "./components/Input/InputTo";
 import { Location } from "./models/Adress";
@@ -8,12 +7,34 @@ import { handleCalcular } from "./utils/handleSumbit";
 const MapComponentLazy = React.lazy(
   () => import("./components/Map/MapComponent")
 );
+
+function Results({ from, to, data }: any) {
+  return (
+    <div>
+      <h1>Resultados</h1>
+      {from.name !== "" && to.name !== "" ? (
+        <>
+          <p>
+            Viaje: {from.name} - {to.name}
+          </p>
+          <p>Tiempo:{Math.round(data.duration / 60)}min </p>
+          <p>Distancia: {Math.round(data.distance / 1000)} km</p>
+          <p>Costo: {data.cost}</p>
+        </>
+      ) : (
+        <p>Ingrese un origen y un destino</p>
+      )}
+    </div>
+  );
+}
+
 export function Tarifa() {
   const initialLocationValue: Location = {
     longitude: 0,
     latitude: 0,
     name: "",
   };
+
   const [from, setFrom] = React.useState<Location>(initialLocationValue);
   const [to, setTo] = React.useState<Location>(initialLocationValue);
   const [data, setData] = React.useState({
@@ -23,39 +44,29 @@ export function Tarifa() {
     cost: "",
   });
 
+  const handleCalculation = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) =>
+      handleCalcular(event, setData, from, to),
+    [from, to]
+  );
+
   return (
     <>
       <div>
         <h1>Calcula tu Tarifa</h1>
         <InputFrom setFrom={setFrom} />
-
         <InputTo setTo={setTo} />
       </div>
-      <button
-        onClick={(event) => handleCalcular(event, setData, from, to)}
-        type="button"
-      >
+
+      <button onClick={handleCalculation} type="button">
         Calcular
       </button>
+
       <Suspense fallback={<div>Loading...</div>}>
         <MapComponentLazy geojson={data.geojson} />
       </Suspense>
 
-      <div>
-        <h1>Resultados</h1>
-        {from.name !== "" && to.name !== "" ? (
-          <>
-            <p>
-              Viaje: {from.name} - {to.name}
-            </p>
-            <p>Tiempo:{Math.round(data.duration / 60)}min </p>
-            <p>Distancia: {Math.round(data.distance / 1000)} km</p>
-            <p>Costo: {data.cost}</p>
-          </>
-        ) : (
-          <p>Ingrese un origen y un destino</p>
-        )}
-      </div>
+      <Results from={from} to={to} data={data} />
     </>
   );
 }
--- AyZTransportes/src/pages/Tarifa/components/Map/MapComponent.tsx
+++ AyZTransportes/src/pages/Tarifa/components/Map/MapComponent.tsx
@@ -1,82 +1,89 @@
 import { useRef, useEffect } from "react";
-import * as maptilersdk from "@maptiler/sdk";
 import "@maptiler/sdk/dist/maptiler-sdk.css";
 import "./map.css";
-
-export default function Map({ geojson }: any) {
-  const mapContainer = useRef(null);
-  const map = useRef<maptilersdk.Map | maplibregl.Map | null>(null);
+import { Map, MapStyle } from "@maptiler/sdk";
+export default function MapComponent({ geojson }: any) {
+  const mapContainer = useRef<HTMLDivElement | null>(null);
+  const map = useRef<Map | null>(null);
   const city = { lng: -68.5251802, lat: -31.5370909 };
   const zoom = 14;
 
   const initializeMap = () => {
-    if (map.current) return; // Evita la inicialización redundante
+    if (!map.current) {
+      map.current = new Map({
+        container: mapContainer.current!,
+        style: MapStyle.BASIC,
+        center: [city.lng, city.lat],
+        zoom: zoom,
+      });
 
-    // Inicializa el mapa
-    map.current = new maptilersdk.Map({
-      container: mapContainer.current!,
-      style: maptilersdk.MapStyle.STREETS,
-      center: [city.lng, city.lat],
-      zoom: zoom,
-    });
-
-    // Resto de la lógica de inicialización del mapa...
+      // Rest of the map initialization logic...
+    }
   };
 
   useEffect(() => {
-    import("@maptiler/sdk").then((maptilersdk) => {
-      maptilersdk.config.apiKey = import.meta.env.VITE_MAPTILER_KEY;
-      initializeMap();
+    const loadMap = async () => {
+      try {
+        const maptilersdk = await import("@maptiler/sdk");
+        maptilersdk.config.apiKey = import.meta.env.VITE_MAPTILER_KEY;
+        initializeMap();
 
-      map.current?.on("load", () => {
-        if (geojson !== "") {
-          const routeCoordinates = [
-            geojson.coordinates[0],
+        if (map.current && geojson !== "") {
+          map.current?.on("load", () => {
+            if (geojson !== "") {
+              const routeCoordinates = [
+                geojson.coordinates[0],
 
-            geojson.coordinates[geojson.coordinates.length - 1],
-          ];
+                geojson.coordinates[geojson.coordinates.length - 1],
+              ];
 
-          routeCoordinates.forEach((coord) => {
-            if (map.current) {
-              new maptilersdk.Marker({ color: "#FF0000" })
-                .setLngLat(coord)
-                .addTo(map.current);
+              routeCoordinates.forEach((coord) => {
+                if (map.current) {
+                  new maptilersdk.Marker({ color: "#FF0000" })
+                    .setLngLat(coord)
+                    .addTo(map.current);
+                }
+              });
+              map.current?.addSource("route", {
+                type: "geojson",
+                data: {
+                  type: "Feature",
+                  properties: {},
+                  geometry: {
+                    type: "LineString",
+                    coordinates: geojson.coordinates,
+                  },
+                },
+              });
+              map.current?.addLayer({
+                id: "route",
+                type: "line",
+                source: "route",
+                layout: {
+                  "line-join": "round",
+                  "line-cap": "round",
+                },
+                paint: {
+                  "line-color": "#888", // Color of the route line
+                  "line-width": 5, // Width of the route line
+                },
+              });
             }
           });
-          map.current?.addSource("route", {
-            type: "geojson",
-            data: {
-              type: "Feature",
-              properties: {},
-              geometry: {
-                type: "LineString",
-                coordinates: geojson.coordinates,
-              },
-            },
-          });
-          map.current?.addLayer({
-            id: "route",
-            type: "line",
-            source: "route",
-            layout: {
-              "line-join": "round",
-              "line-cap": "round",
-            },
-            paint: {
-              "line-color": "#888", // Color of the route line
-              "line-width": 5, // Width of the route line
-            },
-          });
         }
-      });
+      } catch (error) {
+        console.error("Failed to load MapTiler SDK:", error);
+      }
+    };
 
-      return () => {
-        if (map.current) {
-          map.current.remove();
-          map.current = null;
-        }
-      };
-    });
+    loadMap();
+
+    return () => {
+      if (map.current) {
+        map.current.remove();
+        map.current = null;
+      }
+    };
   }, [geojson]);
 
   return (
