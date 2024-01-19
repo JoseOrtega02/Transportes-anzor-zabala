307608b602a17046ccee8b066fbc311ec4d9e0f1
neytor <ortegajoseweb02@gmail.com>
2024-01-19 10:26:29 -0300

    footer style

--- AyZTransportes/src/pages/Home/Home.tsx
+++ AyZTransportes/src/pages/Home/Home.tsx
@@ -2,13 +2,36 @@ import "./App.css";
 import heroImg from "../../assets/mercedes_benz_van_schoolar.svg";
 import HomeButtons from "./components/HomeButtons";
 import CardHome from "./components/cardHome/CardHome";
-import CardIMG from "../../assets/A_group_of_ten_employees_of_a_ 1.png";
+import CardIMGHero from "../../assets/A_group_of_ten_employees_of_a_ 1.png";
 import SectionEquip from "./components/Section Equipo/SectionEquip";
-const data = {
+const dataCardHero = {
   title: "¿Quienes somos?",
   description:
     "La persona muestra una habilidad notable en comunicación efectiva, destacándose por su capacidad para transmitir ideas de manera clara y concisa.",
+  image: CardIMGHero,
+};
+import CardIMG from "../../assets/driver_van_of_a_scholar_transp 1.png";
+
+const dataEquip = {
+  title: "Jhon Doe",
+  description:
+    "La persona muestra una habilidad notable en comunicación efectiva, destacándose por su capacidad para transmitir ideas de manera clara y concisa. Su creatividad es evidente en la generación de soluciones innovadoras y originales.",
   image: CardIMG,
+};
+const dataEquip2 = {
+  title: "¿Quienes somos?",
+  description:
+    "La persona muestra una habilidad notable en comunicación efectiva, destacándose por su capacidad para transmitir ideas de manera clara y concisa. Su creatividad es evidente en la generación de soluciones innovadoras y originales.",
+  image: CardIMG,
+};
+import CardIMGFloat from "../../assets/driver_van_of_a_scholar_transp_float.jpg";
+const dataFloat = {
+  title: "mecedez benz van",
+  image: CardIMGFloat,
+};
+const dataFloat2 = {
+  title: "mecedez benz van",
+  image: CardIMGFloat,
 };
 export function Home() {
   return (
@@ -21,14 +44,18 @@ export function Home() {
       <img src={heroImg} alt="" className="hero__image" />
 
       <HomeButtons />
-      <CardHome props={data} />
+      <CardHome props={dataCardHero} />
       <SectionEquip
         title="Nuestro Equipo"
         description="lorem ipsum dolor sit amet "
+        data1={dataEquip}
+        data2={dataEquip2}
       />
       <SectionEquip
-        title="Nuestro Equipo"
+        title="Nuestra flota"
         description="lorem ipsum dolor sit amet "
+        data1={dataFloat}
+        data2={dataFloat2}
       />
     </main>
   );
