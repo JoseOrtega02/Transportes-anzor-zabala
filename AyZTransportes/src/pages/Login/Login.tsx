400361c46d5b487b36903383de6e90a38af48501
neytor <ortegajoseweb02@gmail.com>
2024-02-13 20:02:34 -0300

    refactor security login

---
+++ AyZTransportes/src/pages/Login/Login.tsx
@@ -0,0 +1,11 @@
+import Login from "../UpdateTarifa/Auth/Login";
+function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default LoginPage;
